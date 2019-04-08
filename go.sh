820060a3d41a696c0da77a26068604bcc3762399
Douglas Muth <dmuth@dmuth.org>
2019-04-08 18:41:11 -0400

    Now setting public-read ACL.

--- go.sh
+++ go.sh
@@ -16,7 +16,8 @@ pushd $(dirname $0) >/dev/null
 echo "# "
 echo "# Syncing to ${TARGET}..."
 echo "# "
-aws s3 sync . ${TARGET} --exclude ".git/*" --exclude "*.swp" #--delete
+#aws s3 sync . ${TARGET} --exclude ".git/*" --exclude "*.swp" #--delete
+aws s3 sync . ${TARGET} --exclude ".git/*" --exclude "*.swp" --acl public-read #--delete
 
 #aws s3 rm ${TARGET}/.git/ --recursive #--dryrun
 #aws s3 rm ${TARGET}/*.swp #--dryrun
